3102d9389254ced488b450510ff60b98461ac501
Taylor Warren <taylorWarrenCoding@gmail.com>
2019-08-15 18:30:21 -0600

    The real Model Button Component

---
+++ src/components/playlists/PlaylistRedirectButton.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+/*Required Local Props :
+  --spotify_id
+  --display_name
+
+Required Global Props :
+  --API_URL
+  --spotifyJoinPlaylist(playlist){}
+*/
+
+class PlaylistRedirectButton extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      API_URL: "/"
+    };
+  }
+
+  async playlistRedirect(e){
+    await axios.get(`${this.API_URL}playlists`, { params: {spotify_id: e.target.value}})
+    .then(response => {
+      console.log(response);
+      //implement SPOTIFY API CALL function from REDUX
+      this.props.spotifyJoinPlaylist(response.data.data.playlist);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+  render() {
+    return (
+      <>
+        <button value={this.props.spotify_id} onclick={(e) => this.playlistRedirect(e)}> {this.props.display_name} </button>
+      </>
+    )
+  }
+}
+
+export default PlaylistRedirectButton;
