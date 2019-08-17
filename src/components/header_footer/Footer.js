bb925f0304fe86373089979cac7cee055d29a129
Taylor Warren <taylorWarrenCoding@gmail.com>
2019-08-17 08:38:11 -0600

    index.js now has BrowserRouter, LocalStorage, and Redux'

--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
 
+/*Required Local Props :
+  --none
+Required Global Props :
+  --logout()
+  --aboutUsRedirect()
+  --
+*/
+
 class Footer extends Component{
   constructor(props){
     super(props);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from './redux/reducer';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {save, load} from 'redux-localstorage-simple';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(thunk, save())(createStore);
+
+const store = createStoreWithMiddleware(rootReducer, load())
+
+
+ReactDOM.render(<Provider store={store}><BrowserRouter><App /> </BrowserRouter></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
