632674a7bc8c4a59846ea8ed6a8f46716bc55843
Taylor Warren <taylorWarrenCoding@gmail.com>
2019-08-17 08:15:09 -0600

    put in react component code for header and footer

---
+++ src/components/header_footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Footer extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <> </>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/components/header_footer/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Header extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <> </>
+    )
+  }
+}
+
+export default Header;
