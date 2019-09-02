f517c9cd3573bf5d2d53cc33c0bc41f6440a928a
Shelby Miller <shelbycrosbee@gmail.com>
2019-09-02 15:14:09 -0600

    shelby bio photo

--- src/components/bios/Bio.js
+++ src/components/bios/Bio.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
 import './index.css'
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Card, Button } from 'react-bootstrap';
+import shelby from '../images/shelby.jpg'
 
 export default class Bio extends Component {
   render() {
     return (
       <Row className='text'>
-        <Col>Chris Cozlick</Col>
-        <Col>Sara Lewis</Col>
-        <Col>Shelby Miller</Col>
-        <Col>Taylor Warren</Col>
-        <Col>Tom Bawel</Col>
+        <Col className='text'>Chris Cozlick</Col>
+        <Col className='text'>Sara Lewis</Col>
+
+
+        <Col><Card style={{ width: '18rem' }} className="cardForBio">
+          <Card.Img variant="top" src={shelby} />
+          <Card.Body>
+            <Card.Title>Shelby Miller</Card.Title>
+            <Card.Text> 
+            </Card.Text>
+            <Button className="button" href="https://shelby-resume.herokuapp.com/" target="blank">Visit My Website</Button>
+          </Card.Body>
+        </Card>
+        </Col>
+
+
+        <Col className='text'>Taylor Warren</Col>
+        <Col className='text'>Tom Bawel</Col>
       </Row>
     )
   }
