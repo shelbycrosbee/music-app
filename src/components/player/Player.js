061db3a5bef38ff0ff1db3a68c3ba18f1d57796a
Taylor Warren <taylorWarrenCoding@gmail.com>
2019-09-21 16:04:36 -0600

    fixed a mistake

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -198,7 +198,8 @@ class Player extends React.Component {
     await joinOther(playlist_data, deviceId, this.props.token);
     await console.log('Timestamp#1:' + Date.now())
     let seekProgressMS = parseInt(playlist_data.progress_ms) - parseInt(playlist_data.join_time) + Date.now();
-    this.player.seek(seekProgressMS).then(() => console.log(seekProgressMS + .51 * 1000))
+    setTimeout(this.player.togglePlay(), 3000)
+    this.player.seek(seekProgressMS + 3 * 1000).then(() => console.log(seekProgressMS))
     await console.log('Timestamp#2:' + Date.now())
 
     // this.player.pause();
