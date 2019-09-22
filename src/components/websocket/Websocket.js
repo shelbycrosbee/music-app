c8e8dca475cd1953bf330254f1fb01823a8769d3
Taylor Warren <taylorWarrenCoding@gmail.com>
2019-09-22 08:51:07 -0600

    fixed async

--- src/components/websocket/Websocket.js
+++ src/components/websocket/Websocket.js
@@ -43,7 +43,7 @@ class Websocket extends React.Component {
       this.setState({ mayIAsk: false })
     }
     if(this.props.changeSong === true){
-      const playlist_data = await this.props.getPosition();
+      const playlist_data = this.props.getPosition();
         // console.log(`Time_00: ${time_00} -- Time_01: ${playlist_data.time_01} -- Time_03: ${time_02}`);
         //api call to websocket
         this.state.playlist.emit('giveGlobalPosition', { playlist: { ...playlist_data, join_time: Date.now()}});
